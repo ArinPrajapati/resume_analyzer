5baedb18e69c1a89be06d4e711b72d385bd76312
ArinPrajapati <arinprajapati78@gmail.com>
2025-08-08 09:38:27 +0530

    added route for analyze

--- app/routes/analyze.py
+++ app/routes/analyze.py
@@ -1,3 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, Form, HTTPException, UploadFile
+from time import sleep
+
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
+
+
+@router.post("/analyze")
+async def analyze_resume(
+    resume_file: UploadFile = File(...),
+    jd_text: UploadFile = File(...),
+):
+    if not resume_file.content_type == "application/pdf":
+        raise HTTPException(status_code=400, detail="Resume must be a PDF file")
+    if not jd_text.content_type == "application/pdf":
+        raise HTTPException(status_code=400, detail="Resume must be a PDF file")
+
+    sleep(3)
+
+    return JSONResponse(
+        {
+            "match_score": 0,
+            "missing_skills": [],
+            "strengths": [],
+            "recommendation": "Processing logic coming soon...",
+        }
+    )
